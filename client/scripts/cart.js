eea6770ef8fb329a679c4cab33c9c0ecc5a73ff3
Emilio <erix@jsjutsu.com>
2016-01-24 15:17:30 +0100

    Add cart view

---
+++ client/scripts/cart.js
@@ -0,0 +1,15 @@
+Template.cart.helpers({
+  items: function(){
+    return Cart.items();
+  },
+  numItems: function(){
+    return Cart.numItems();
+  }
+});
+
+/*
+_id: "hwmxBQdzSYmHShLFb"
+quantity: 1
+relationId: "569019022a1195022eb52132"
+relationType: "Products"
+*/
