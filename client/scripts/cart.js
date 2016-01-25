a0b45eecd0df0bef4b1eb411cc67289fed3d4362
Emilio <erix@jsjutsu.com>
2016-01-25 19:50:21 +0100

    Prepared cart page to pay

--- client/scripts/cart.js
+++ client/scripts/cart.js
@@ -1,12 +1,35 @@
 Template.cart.helpers({
   items: function(){
-    console.log(Cart.items());
-    return Cart.items();
+    var items = [];
+    Cart.items().forEach(function(item) {
+      items.push({
+        _id: item.relationId,
+        item: Products.findOne({_id: new Meteor.Collection.ObjectID(item.relationId)}),
+        amount: item.quantity
+      });
+    });
+    console.log(items);
+    return items;
   },
   numItems: function(){
     return Cart.numItems();
+  },
+  totalPrice: function() {
+    var price = 0;
+    Cart.items().forEach(function(item) {
+      var product = Products.findOne({_id: new Meteor.Collection.ObjectID(item.relationId)});
+      price += item.quantity * product.price;
+    });
+    return price;
   }
 });
+
+Template.cart.events({
+  "click #pay": function(event, template){
+    alert('pagando!');
+  }
+});
+
 
 /*
 _id: "hwmxBQdzSYmHShLFb"
